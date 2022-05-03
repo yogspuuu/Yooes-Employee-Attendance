bacabf7e4be2929686e880311f83d571a25223b5
yogspuuu <yogapupratama@gmail.com>
2022-05-04 01:04:02 +0700

    change recognition validation from video capture to image read

--- engine/api/__init__.py
+++ engine/api/__init__.py
@@ -1,14 +1,24 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Config for upload files   
+UPLOAD_FOLDER = f'{os.getcwd()}/../storage/app/recognition_image'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+# def allowed_file(filename):
+#     return '.' in filename and \
+#         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Config for database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../yooes.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Config for blueprint
 from api.recognition.views import recognition
 app.register_blueprint(recognition)
-
 db.create_all()
--- engine/api/recognition/views.py
+++ engine/api/recognition/views.py
@@ -1,8 +1,10 @@
-import json
+import base64
 from api import db, app
 from flask.views import MethodView
 from api.recognition.models import User
-from flask import request, jsonify, Blueprint, abort
+from flask import request, jsonify, Blueprint
+from api.recognition.utils import b64_to_image
+from api.recognition.recognition import do_recognition
 
 recognition = Blueprint('recognition', __name__)
 
@@ -22,7 +24,7 @@ class RegisterUserFaces(MethodView):
         user = User(name)
         db.session.add(user)
         db.session.commit()
-        
+
         return jsonify({user.id: {
             'name': user.name,
         }})
@@ -39,17 +41,28 @@ class RegisterUserFaces(MethodView):
 
 class ValidateUserFaces(MethodView):
     def get(self, id=None):
-        user = User.query.filter_by(id=id).first()
-        if not user:
-            abort(404)
-        res = {
-            'name': user.name,
-        }
-        return jsonify(res)
-
-    def post(self, id):
-        # Post the record for the provided id.
+        # Get the record for the provided id.
         return
+
+    def post(self, id=None):
+        # get requested base64 image data
+        base64_image = request.form.get('base64_image')
+        
+        # convert base64 to image
+        image = b64_to_image(base64_image)
+
+        # show result of prediction
+        result_of_prediction = do_recognition(image)
+
+        # user = User.query.filter_by(id=id).first()
+        # if not user:
+        #     abort(404)
+        res = {
+            'id': result_of_prediction,
+            # 'name': user.name,
+        }
+        
+        return jsonify(res)
 
     def put(self, id):
         # Update the record for the provided id
@@ -62,11 +75,11 @@ class ValidateUserFaces(MethodView):
 
 
 register_faces = RegisterUserFaces.as_view('register_faces')
-valdiate_vaces = ValidateUserFaces.as_view('valdiate_vaces')
+valdiate_faces = ValidateUserFaces.as_view('valdiate_vaces')
 
 app.add_url_rule(
     '/recognition/register/faces', view_func=register_faces, methods=['POST']
 )
 app.add_url_rule(
-    '/recognition/validate/faces/<int:id>', view_func=valdiate_vaces, methods=['GET']
+    '/recognition/validate/faces', view_func=valdiate_faces, methods=['POST']
 )
